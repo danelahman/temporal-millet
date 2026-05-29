a6811cb7f68cdb397f3787f3787f6c505de877d3
Danel Ahman <danel.ahman@gmail.com>
2026-05-29 23:36:28 +0300

    Added effect grades to AST (function and handler types, operaton signatures)

--- src/01-language/ast.ml
+++ src/01-language/ast.ml
@@ -1,8 +1,3 @@
-(** Grade-independent definitions, shared across all grade-system
-    instantiations. Grouped in one module so that [Make] can re-export them with
-    a single [include], and so that every [Make (GS)] sees the *same* symbol
-    modules and type names (e.g.
-    [Make (GS1).Variable.t = Make (GS2).Variable.t]). *)
 module Common = struct
   module Symbol = Utils.Symbol
   module Variable = Symbol.Make ()
@@ -57,7 +52,7 @@ module Make (GS : GradeSystem.S) = struct
   include Common
 
   type effect_grade =
-    | EffectGradeConst of GS.ResourceGrade.t
+    | EffectGradeConst of GS.EffectGrade.t
     | EffectGradeParam of effect_grade_param
     | EffectGradeAdd of effect_grade * effect_grade
 
@@ -65,6 +60,7 @@ module Make (GS : GradeSystem.S) = struct
     | ResourceGradeConst of GS.ResourceGrade.t
     | ResourceGradeParam of resource_grade_param
     | ResourceGradeAdd of resource_grade * resource_grade
+    | ResourceGradeOfEffect of effect_grade
 
   type ty =
     | TyConst of Const.ty
@@ -75,7 +71,7 @@ module Make (GS : GradeSystem.S) = struct
     | TyBox of resource_grade * ty  (** [ [rho]ty ] *)
     | TyHandler of comp_ty * comp_ty
 
-  and comp_ty = CompTy of ty * resource_grade  (** [ty ! rho] *)
+  and comp_ty = CompTy of ty * effect_grade  (** [ty ! rho] *)
 
   type pattern =
     | PVar of variable
@@ -114,7 +110,7 @@ module Make (GS : GradeSystem.S) = struct
 
   type command =
     | TyDef of (ty_param list * ty_name * ty_def) list
-    | OpSig of (operation * ty * ty * resource_grade)
+    | OpSig of (operation * ty * ty * effect_grade)
     | TopLet of variable * expression
     | TopDo of computation
     | Grades of string
@@ -125,95 +121,111 @@ module Make (GS : GradeSystem.S) = struct
 
   type 'a context = 'a context_elem_ty list
 
-  let rec substitute_resource_grade subst = function
+  let rec substitute_effect_grade eps_subst = function
+    | EffectGradeConst _ as eps -> eps
+    | EffectGradeParam ep as eps -> (
+        match EffectGradeParamMap.find_opt ep eps_subst with
+        | None -> eps
+        | Some eps' -> eps')
+    | EffectGradeAdd (eps1, eps2) ->
+        EffectGradeAdd
+          ( substitute_effect_grade eps_subst eps1,
+            substitute_effect_grade eps_subst eps2 )
+
+  let rec substitute_resource_grade rho_subst eps_subst = function
     | ResourceGradeConst _ as rho -> rho
     | ResourceGradeParam tp as rho -> (
-        match ResourceGradeParamMap.find_opt tp subst with
+        match ResourceGradeParamMap.find_opt tp rho_subst with
         | None -> rho
         | Some rho' -> rho')
     | ResourceGradeAdd (rho, rho') ->
         ResourceGradeAdd
-          ( substitute_resource_grade subst rho,
-            substitute_resource_grade subst rho' )
+          ( substitute_resource_grade rho_subst eps_subst rho,
+            substitute_resource_grade rho_subst eps_subst rho' )
+    | ResourceGradeOfEffect eps ->
+        ResourceGradeOfEffect (substitute_effect_grade eps_subst eps)
 
-  let rec substitute_ty ty_subst rho_subst = function
+  let rec substitute_ty ty_subst rho_subst eps_subst = function
     | TyConst _ as ty -> ty
     | TyParam a as ty -> (
         match TyParamMap.find_opt a ty_subst with None -> ty | Some ty' -> ty')
     | TyApply (ty_name, tys) ->
-        TyApply (ty_name, List.map (substitute_ty ty_subst rho_subst) tys)
-    | TyTuple tys -> TyTuple (List.map (substitute_ty ty_subst rho_subst) tys)
-    | TyArrow (ty1, CompTy (ty2, rho)) ->
+        TyApply
+          (ty_name, List.map (substitute_ty ty_subst rho_subst eps_subst) tys)
+    | TyTuple tys ->
+        TyTuple (List.map (substitute_ty ty_subst rho_subst eps_subst) tys)
+    | TyArrow (ty1, cty2) ->
         TyArrow
-          ( substitute_ty ty_subst rho_subst ty1,
-            CompTy
-              ( substitute_ty ty_subst rho_subst ty2,
-                substitute_resource_grade rho_subst rho ) )
+          ( substitute_ty ty_subst rho_subst eps_subst ty1,
+            substitute_comp_ty ty_subst rho_subst eps_subst cty2 )
     | TyBox (rho, ty) ->
         TyBox
-          ( substitute_resource_grade rho_subst rho,
-            substitute_ty ty_subst rho_subst ty )
-    | TyHandler (CompTy (ty1, rho1), CompTy (ty2, rho2)) ->
+          ( substitute_resource_grade rho_subst eps_subst rho,
+            substitute_ty ty_subst rho_subst eps_subst ty )
+    | TyHandler (cty1, cty2) ->
         TyHandler
-          ( CompTy
-              ( substitute_ty ty_subst rho_subst ty1,
-                substitute_resource_grade rho_subst rho1 ),
-            CompTy
-              ( substitute_ty ty_subst rho_subst ty2,
-                substitute_resource_grade rho_subst rho2 ) )
+          ( substitute_comp_ty ty_subst rho_subst eps_subst cty1,
+            substitute_comp_ty ty_subst rho_subst eps_subst cty2 )
 
-  let substitute_comp_ty ty_subst rho_subst = function
-    | CompTy (ty, rho) ->
+  and substitute_comp_ty ty_subst rho_subst eps_subst = function
+    | CompTy (ty, eps) ->
         CompTy
-          ( substitute_ty ty_subst rho_subst ty,
-            substitute_resource_grade rho_subst rho )
+          ( substitute_ty ty_subst rho_subst eps_subst ty,
+            substitute_effect_grade eps_subst eps )
+
+  let rec free_effect_grades = function
+    | EffectGradeConst _ -> EffectGradeParamSet.empty
+    | EffectGradeParam ep -> EffectGradeParamSet.singleton ep
+    | EffectGradeAdd (eps1, eps2) ->
+        EffectGradeParamSet.union (free_effect_grades eps1)
+          (free_effect_grades eps2)
+
+  let rec free_resource_grades = function
+    | ResourceGradeConst _ ->
+        (ResourceGradeParamSet.empty, EffectGradeParamSet.empty)
+    | ResourceGradeParam a ->
+        (ResourceGradeParamSet.singleton a, EffectGradeParamSet.empty)
+    | ResourceGradeAdd (l, r) ->
+        let rho_l, eps_l = free_resource_grades l in
+        let rho_r, eps_r = free_resource_grades r in
+        ( ResourceGradeParamSet.union rho_l rho_r,
+          EffectGradeParamSet.union eps_l eps_r )
+    | ResourceGradeOfEffect eps ->
+        (ResourceGradeParamSet.empty, free_effect_grades eps)
+
+  let empty_free_vars =
+    (TyParamSet.empty, ResourceGradeParamSet.empty, EffectGradeParamSet.empty)
+
+  let union_free_vars (ty1, rho1, eps1) (ty2, rho2, eps2) =
+    ( TyParamSet.union ty1 ty2,
+      ResourceGradeParamSet.union rho1 rho2,
+      EffectGradeParamSet.union eps1 eps2 )
 
   let rec free_vars = function
-    | TyConst _ -> (TyParamSet.empty, ResourceGradeParamSet.empty)
-    | TyParam a -> (TyParamSet.singleton a, ResourceGradeParamSet.empty)
-    | TyApply (_, tys) ->
+    | TyConst _ -> empty_free_vars
+    | TyParam a ->
+        ( TyParamSet.singleton a,
+          ResourceGradeParamSet.empty,
+          EffectGradeParamSet.empty )
+    | TyApply (_, tys) | TyTuple tys ->
         List.fold_left
-          (fun (ty_params, rho_params) ty ->
-            let fv_ty, fv_rho = free_vars ty in
-            ( TyParamSet.union ty_params fv_ty,
-              ResourceGradeParamSet.union rho_params fv_rho ))
-          (TyParamSet.empty, ResourceGradeParamSet.empty)
-          tys
-    | TyTuple tys ->
-        List.fold_left
-          (fun (ty_params, rho_params) ty ->
-            let fv_ty, fv_rho = free_vars ty in
-            ( TyParamSet.union ty_params fv_ty,
-              ResourceGradeParamSet.union rho_params fv_rho ))
-          (TyParamSet.empty, ResourceGradeParamSet.empty)
-          tys
-    | TyArrow (ty1, CompTy (ty2, rho)) ->
-        let fv_ty1, fv_rho1 = free_vars ty1 in
-        let fv_ty2, fv_rho2 = free_vars ty2 in
-        let nested_free_rhos = free_resource_grades rho in
-        ( TyParamSet.union fv_ty1 fv_ty2,
-          ResourceGradeParamSet.union
-            (ResourceGradeParamSet.union fv_rho1 fv_rho2)
-            nested_free_rhos )
+          (fun acc ty -> union_free_vars acc (free_vars ty))
+          empty_free_vars tys
+    | TyArrow (ty, cty) ->
+        union_free_vars (free_vars ty) (free_comp_ty_vars cty)
     | TyBox (rho, ty) ->
-        let fv_ty, fv_rho = free_vars ty in
-        let nested_free_rhos = free_resource_grades rho in
-        (fv_ty, ResourceGradeParamSet.union fv_rho nested_free_rhos)
-    | TyHandler (CompTy (ty1, rho1), CompTy (ty2, rho2)) ->
-        let fv_ty1, fv_rho1 = free_vars ty1 in
-        let fv_ty2, fv_rho2 = free_vars ty2 in
-        let nested_free_rhos1 = free_resource_grades rho1 in
-        let nested_free_rhos2 = free_resource_grades rho2 in
-        ( TyParamSet.union fv_ty1 fv_ty2,
-          ResourceGradeParamSet.union
-            (ResourceGradeParamSet.union fv_rho1 fv_rho2)
-            (ResourceGradeParamSet.union nested_free_rhos1 nested_free_rhos2) )
+        let fv_ty, fv_rho, fv_eps = free_vars ty in
+        let nested_rho, nested_eps = free_resource_grades rho in
+        ( fv_ty,
+          ResourceGradeParamSet.union fv_rho nested_rho,
+          EffectGradeParamSet.union fv_eps nested_eps )
+    | TyHandler (cty1, cty2) ->
+        union_free_vars (free_comp_ty_vars cty1) (free_comp_ty_vars cty2)
 
-  and free_resource_grades rho =
-    match rho with
-    | ResourceGradeConst _ -> ResourceGradeParamSet.empty
-    | ResourceGradeParam a -> ResourceGradeParamSet.singleton a
-    | ResourceGradeAdd (l, r) ->
-        ResourceGradeParamSet.union (free_resource_grades l)
-          (free_resource_grades r)
+  and free_comp_ty_vars = function
+    | CompTy (ty, eps) ->
+        let fv_ty, fv_rho, fv_eps = free_vars ty in
+        ( fv_ty,
+          fv_rho,
+          EffectGradeParamSet.union fv_eps (free_effect_grades eps) )
 end
