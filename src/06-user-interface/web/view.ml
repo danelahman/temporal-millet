cd0bc784c48b6e23aacebd73830f85d051e413e7
Danel Ahman <danel.ahman@gmail.com>
2026-09-12 14:28:22 +0100

    Nits in UI

--- src/06-user-interface/web/view.ml
+++ src/06-user-interface/web/view.ml
@@ -161,7 +161,7 @@ let view_compiler (model : Model.model) =
               onclick (fun _ -> Model.RunCode);
               (* disabled (Result.is_error model.loaded_code); *)
             ]
-          [ text "Compile & run" ];
+          [ text "Typecheck & run" ];
         (match model.run_model with
         | Error msg -> elt "p" ~a:[ class_ "help is-danger" ] [ text msg ]
         | Ok _ -> nil);
@@ -295,6 +295,40 @@ let run_view (run_model : Model.run_model) =
   in
   view_contents (active_view @ completed_views) [ view_steps run_model steps ]
 
+let github_url = "https://github.com/danelahman/temporal-millet"
+
+(* The GitHub mark, as in GitHub's Octicons (MIT licensed), drawn in the
+   current text colour so that it follows the link's hover styling. *)
+let github_mark =
+  svg_elt "svg"
+    ~a:
+      [
+        attr "viewBox" "0 0 16 16";
+        attr "width" "20";
+        attr "height" "20";
+        attr "aria-hidden" "true";
+      ]
+    [
+      svg_elt "path"
+        ~a:
+          [
+            attr "fill" "currentColor";
+            attr "d"
+              "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 \
+               7.59.4.07.55-.17.55-.38 \
+               0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 \
+               1.08.58 1.23.82.72 1.21 1.87.87 \
+               2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 \
+               0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 \
+               2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 \
+               2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 \
+               3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 \
+               1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 \
+               8c0-4.42-3.58-8-8-8z";
+          ]
+        [];
+    ]
+
 let view_navbar =
   let view_title =
     div
@@ -303,6 +337,19 @@ let view_navbar =
         elt "a"
           ~a:[ class_ "navbar-item" ]
           [ elt "p" ~a:[ class_ "title" ] [ text "Temporal Millet" ] ];
+        elt "a"
+          ~a:
+            [
+              class_ "navbar-item github-link";
+              attr "href" github_url;
+              attr "target" "_blank";
+              attr "rel" "noopener";
+              attr "title" "Source code on GitHub";
+            ]
+          [
+            elt "span" ~a:[ class_ "icon" ] [ github_mark ];
+            elt "span" [ text "GitHub" ];
+          ];
       ]
   in
 
